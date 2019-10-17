e9d0469277e86834f8e3b7d8f718b0fb90984bf0
Daniel Alarcon <dannydk6@neogenesis.local>
2019-10-17 17:01:15 -0400

    auto-update

--- api/Leaderboard.py
+++ api/Leaderboard.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 API_URL = 'http://ds-leaderboards.com:5000'
 #API_URL = 'http://localhost:5000'
@@ -14,15 +15,41 @@ def signup(username=None,
 
 
 def submit_question(
-                    qnumber,
-                    response,
-                    username=None,
-                    password=None
-                    ):
+    qnumber,
+    response,
+    username=None,
+    password=None
+):
     body = {'username': username,
             'password': password,
             'qnumber': qnumber,
             'response': response}
     r = requests.post(f'{API_URL}/api/question_attempt', json=body)
     resp = r.json()
-    print(resp)
+    if type(resp) is str:
+        print(resp)
+    elif type(resp) is dict and 'is_correct' in resp:
+        is_correct = 'correct' if resp['is_correct'] else 'incorrect'
+        print(f'Your answer is {is_correct}')
+
+
+def submit_four(answer, username, password):
+    try:
+        parsed = json.dumps([int(x) for x in list(answer.values)])
+    except:
+        return 'Error parsing. Please make sure you are sending a series of ints.'
+    submit_question(qnumber=4,
+                    response=parsed,
+                    username=username,
+                    password=password)
+    
+def submit_six(answer, username, password):
+    try:
+        parsed = json.dumps([str(x) for x in list(answer.values)])
+    except:
+        return 'Error parsing. Please make sure you are sending a series of ints.'
+    submit_question(qnumber=6,
+                    response=parsed,
+                    username=username,
+                    password=password)
+
